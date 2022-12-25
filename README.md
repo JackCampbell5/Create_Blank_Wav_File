8ababcad94c25d8f9d7917aee03107212a220476
BuildTools <Jackpurple8@gmail.com>
2022-12-25 18:43:11 -0500

    Added all the info to README.md Updated main.py with more comments

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Create_Blank_Wav_File
+
+## What it does:
++ This code takes the data from left_channel.py and right_channel.py and creates a blank audio file
++ The output will be 6 minutes and 58 seconds as that is the max can be created
++ It will be placed in a directory of your choice
++ It is supposed to be used in conjunction with [Wave Separate Left Right]{}
+
+## How to run:
++ Install dependency's
++ Set directory to output file
++ Run the code
++ Run Wave Separate Left Right if you choose with output file
+
+## Sources used to create:
++ [Wave documentation](https://docs.python.org/3/library/wave.html "Wave Site")
++ [Numpy documentation](https://numpy.org/doc/stable/index.html "NumPy documentation")
+
+## Dependency's:
++ Numpy
++ Wave
++ Os
+
+### Todo:
++ Add link to wave separate
--- main.py
+++ main.py
@@ -5,39 +5,35 @@ import numpy as np
 from left_channel import get_left_channel
 from right_channel import get_right_channel
 
-directory = r"C:\Users\Jack Campbell\Documents\Sound project Help Files\Python Code\TestFiles\More"
 
 # Set the directory containing the wav files
-# directory = r"C:\Users\Jack Campbell\Documents\Sound project Help Files\Python Code\TestFiles\More"
-print(get_left_channel()[:10])
-print(get_right_channel()[:10])
+directory = r"C:\Users\jackp\Documents\Game Design\SoundProject\Audio\Python Code\TestFiles"
 
-# Split the wave data into left and right channels
+# Get the wave data from left and right channels
 left_channel = get_left_channel()
 right_channel = get_right_channel()
 
-
 # Need to be created in a 2*2 formation3
-wave_data_left = np.array([left_channel, right_channel])
-wave_data_left = np.reshape(wave_data_left, len(left_channel) * 2, order='F')
-wave_data_left = np.reshape(wave_data_left, (len(left_channel), 2))
+wave_data_blank = np.array([left_channel, right_channel])
+wave_data_blank = np.reshape(wave_data_blank, len(left_channel) * 2, order='F')
+wave_data_blank = np.reshape(wave_data_blank, (len(left_channel), 2))
 
-# Make the paths for writing left and right
-path_left = os.path.join(directory, "blank.wav")
+# Make the paths for writing files
+path_blank = os.path.join(directory, "blank.wav")
 
 # Create file for left
-outwav_left = wave.open(path_left, 'w')
+outwav_blank = wave.open(path_blank, 'w')
 
 # Set stereo vs mono
-outwav_left.setnchannels(2)
-outwav_left.setsampwidth(2)
-outwav_left.setframerate(48000)
-outwav_left.setnframes(57600000)
-outwav_left.setcomptype('NONE', 'not compressed')
+outwav_blank.setnchannels(2)
+outwav_blank.setsampwidth(2)
+outwav_blank.setframerate(48000)
+outwav_blank.setnframes(57600000)
+outwav_blank.setcomptype('NONE', 'not compressed')
 
 
-# Writes the ou tput file
-outwav_left.writeframes(wave_data_left.tobytes())
+# Writes the output file
+outwav_blank.writeframes(wave_data_blank.tobytes())
 
-# We are done
-outwav_left.close()
+# Done and close it
+outwav_blank.close()
